2819c3f17dedbb1e783e3dde39df9823a50106ff
Atharva Patil <65209968+atharva-p@users.noreply.github.com>
2022-07-29 19:52:07 +0530

    added problem statement to code

--- problems/leet1523.c
+++ problems/leet1523.c
@@ -1,4 +1,5 @@
-// problem solved on 29th july
+// Given two non - negative integers low and high.Return the count of odd numbers between low and high(inclusive).
+
 #include <stdio.h>
 #include <string.h>
 
