ffca61308e42f550116832cc6475cc0233a4a10d
Atharva Patil <65209968+atharva-p@users.noreply.github.com>
2022-08-01 00:27:37 +0530

    added a test file

--- problems/leet1480.c
+++ problems/leet1480.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-// int runningSum(int* nums[], int* numsSize, int* returnSize)
-// {
-//     for (int i = 0; i < numsSize; i++)
-//     { // i = 2
-//         for (int j = i - 1; j >= 0; j--)
-//         {
-//             nums[i] = nums[i] + nums[j];
-//         }
-//     }
-//     return nums;
-// }
-
-int runningSum(int *nums[])
+void runningSum(int *array, int size)
 {
-    return *nums;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            array[i] = array[i] + array[j];
+            printf("within loop for i = %d sum is %d \n", i, array[i]);
+        }
+    }
 }
 
 int main()
 {
-    int nums[] = {1, 2, 3, 4};
-    int numsSize = 4;
-    int returnSize = 0;
-
-    runningSum(&nums);
+    int array[] = {1, 2, 3, 4};
+    int size = sizeof(array) / sizeof(array[0]);
+    printf("array before modification\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    runningSum(&array[0], size);
+    printf("\narray after modification");
+    for (int k = 0; k < size; k++)
+    {
+        printf("%d ", array[k]); // 1 2 3 4 -> 1 3 6 10
+    }
     return 0;
 }
