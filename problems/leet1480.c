638baa0ab3834c39db00a2b04a68f674da6c3f4c
Atharva Patil <65209968+atharva-p@users.noreply.github.com>
2022-08-04 12:05:21 +0530

    added malloc to the code

--- problems/leet1480.c
+++ problems/leet1480.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void runningSum(int *array, int size)
 {
-    int answer[5];
-    answer[0] = array[0];
+    int *answer = (int *)malloc(size * sizeof(int));
+    answer[0] = array[0]; // answer[0] = *(answer + 0) and answer[i] = *(answer + i);
     for (int i = 1; i < size; i++)
     {
-        answer[i] = array[i] + answer[i - 1];
+        answer[i] = array[i] + answer[i - 1]; // array[i] = *(array + i)
     }
     printf("running sum array\n");
     for (int i = 0; i < size; i++)
     {
         printf("%d ", answer[i]);
     }
+    free(answer); // for later: return first and then free
 }
 
 int main()
 {
-    int nums[] = {3, 1, 2, 10, 1};
+    int nums[] = {1, 1, 1, 1, 1};
     int size = sizeof(nums) / sizeof(nums[0]);
     runningSum(&nums[0], size);
     return 0;
